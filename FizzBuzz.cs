b4f55b0ea4ecedc58eedba1f3274acbe4acb6746
Łukasz Smak <lukasz.smak@gmail.com>
2020-07-27 22:50:12 +0200

    Add project files.

---
+++ FizzBuzz.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlay
+{
+    class FizzBuzz
+    {
+        public string DoFizzBuzz(int number)
+        {
+            string str = "";
+
+            if (number % 3 == 0)
+                str += "Fizz";
+
+
+            if (number % 5 == 0)
+                str += "Buzz";
+
+
+            if (str.Length == 0)
+            {
+                Console.WriteLine();
+                str = number.ToString();
+            }
+
+            return str;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzzPlay
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Witaj w zabawie o tajemniczej nazwie 'FizzBuzz'!\nPodaj dowolną liczbę całkowtitą i zobacz co zrobi program: ");
+
+            try
+            {
+                if (!int.TryParse(Console.ReadLine(), out int userNumber))
+                    throw new Exception("\nNie podałeś liczby całkowitej!");
+
+                var fizzBuzz = new FizzBuzz();
+
+                var result = fizzBuzz.DoFizzBuzz(userNumber);
+
+                Console.WriteLine("\nWynik zabawy w 'FizzBuzz' to: ");
+                Console.WriteLine(result);
+            }
+
+            catch (Exception ex)
+            {
+                //Logowanie do pliku
+                Console.WriteLine(ex.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("\nDziękuję za udział w zabawie.\nNaciśnij dowolny klawisz, aby zakończyć program i wyjść.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
